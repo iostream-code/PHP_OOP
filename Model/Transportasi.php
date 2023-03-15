5a170fb19a0ab6078170808fec788d06c05a8227
iostream-code <aryakmifjannah2603@gmail.com>
2023-03-15 17:00:02 +0700

    Third Commit

--- Model/Transportasi.php
+++ Model/Transportasi.php
@@ -5,14 +5,27 @@ class Transportasi
     public $jenis;
     public $bahanBakar;
     public $jumlahPenumpang;
+    protected $merk;
 
     //Method
-    function set_jenis($jenis){
+    function set_jenis($jenis)
+    {
         $this->jenis = $jenis;
     }
 
-    function get_jenis(){
+    function get_jenis()
+    {
         return $this->jenis;
+    }
+
+    function set_merk($merk)
+    {
+        $this->merk = $merk;
+    }
+
+    function get_merk()
+    {
+        return $this->merk;
     }
 }
 
@@ -22,7 +35,11 @@ $sepedaMotor1 = new Transportasi();
 $mobil1->set_jenis("Transportasi Darat");
 $sepedaMotor1->set_jenis("Transportasi Darat");
 
+$mobil1->set_merk("Sedan");
+$sepedaMotor1->set_merk("Ducati");
+
+echo $mobil1->get_merk() . " adalah\t";
 echo $mobil1->get_jenis();
 echo "<br>";
-echo $sepedaMototor1->get_jenis();
-
+echo $sepedaMotor1->get_merk() . " adalah\t";
+echo $sepedaMotor1->get_jenis();
