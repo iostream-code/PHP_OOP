33e29bb3c0bd405e617bfd00bd4bf9613d18117d
iostream-code <aryakmifjannah2603@gmail.com>
2023-03-15 16:51:21 +0700

    Second Commit

--- Model/Transportasi.php
+++ Model/Transportasi.php
@@ -1,7 +1,28 @@
 <?php
 class Transportasi
 {
+    //Properties
     public $jenis;
     public $bahanBakar;
     public $jumlahPenumpang;
+
+    //Method
+    function set_jenis($jenis){
+        $this->jenis = $jenis;
+    }
+
+    function get_jenis(){
+        return $this->jenis;
+    }
 }
+
+$mobil1 = new Transportasi();
+$sepedaMotor1 = new Transportasi();
+
+$mobil1->set_jenis("Transportasi Darat");
+$sepedaMotor1->set_jenis("Transportasi Darat");
+
+echo $mobil1->get_jenis();
+echo "<br>";
+echo $sepedaMototor1->get_jenis();
+
